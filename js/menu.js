f88b92366ca9d65d2fd734b522644953839935e8
Justo Iribarren <jusmaliri@gmail.com>
2023-02-18 20:59:15 -0300

    Menu Update

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,7 @@
     const openButtom = document.querySelector(".nav__menu");
     const menu = document.querySelector(".nav__list");
     const closeMenu = document.querySelector(".nav__close");
-    const linkMenu = document.querySelector(".nav__link");
-    linkMenu1 = document.querySelector(".sobremi"),
-    linkMenu2 = document.querySelector(".proyectos"),
-    linkMenu3 = document.querySelector(".contacto");
+    const linkMenu = document.querySelectorAll(".nav__link");
 
     openButtom.addEventListener("click", ()=>{
         menu.classList.add("nav__list--show");
@@ -15,8 +12,11 @@
         menu.classList.remove("nav__list--show");
     });
 
-    linkMenu.addEventListener("click", ()=>{menu.classList.remove("nav__list--show");});
-    linkMenu1.addEventListener("click", ()=>{menu.classList.remove("nav__list--show");});
-    linkMenu2.addEventListener("click", ()=>{menu.classList.remove("nav__list--show");});
-    linkMenu.addEventListener("click", function(){menu.classList.remove("nav__list--show");});
+
+    linkMenu.forEach((link) => {
+         link.addEventListener("click", ()=>{
+            menu.classList.remove("nav__list--show");
+        }); 
+    });
+
 })();
